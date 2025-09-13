3a7f0d08f0b2a491bd6a131d195abef5df15c588
NisargRamani05 <nisargramani05@gmail.com>
2025-09-13 21:44:08 +0530

    Initial project setup

---
+++ script.js
@@ -0,0 +1,5 @@
+// Handle contact form submission
+document.getElementById("contactForm").addEventListener("submit", function(e){
+  e.preventDefault(); // Prevent page reload
+  alert("Thank you! Your message has been sent.");
+});
